654c6445e40eda432e71a1cb014c955b8406b1d9
Гульшат <gayfullinagulshat@yandex.ru>
2025-08-28 18:36:26 +0300

    fixed user create serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -48,13 +48,6 @@ class UserCreateSerializer(UserCreate):
             'password'
         )
 
-    def create(self, validated_data):
-        password = validated_data.pop('password')
-        user = User.objects.create(**validated_data)
-        user.set_password(password)
-        user.save()
-        return user
-
 
 class UserSerializer(UserDetail):
     """Сериализатор пользователя."""
