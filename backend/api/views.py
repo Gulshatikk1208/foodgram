fb2ce063975f14295ec66a4fe439f34aa6929547
Гульшат <gayfullinagulshat@yandex.ru>
2025-08-28 23:08:30 +0300

    fix

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,7 @@
-from django.conf import settings
+# from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.db.models import Sum
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import mixins, status, viewsets
