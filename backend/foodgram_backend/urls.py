84388ea087a80d80660e8c6a451f9a41cf601185
Гульшат <gayfullinagulshat@yandex.ru>
2025-08-29 00:13:45 +0300

    test 394934

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 from django.urls import include, path
 
-from api.views import redirect_to_recipe
+# from api.views import redirect_to_recipe
+
+
+# Временная тестовая функция
+def test_redirect_view(request, pk):
+    print(f"TEST REDIRECT: pk={pk}")
+    return HttpResponseRedirect('https://google.com')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('s/<int:pk>/', redirect_to_recipe, name='recipe-short-link'),
+    path('test-redirect/<int:pk>/', test_redirect_view)
+    # path('s/<int:pk>/', redirect_to_recipe, name='recipe-short-link'),
 ]
