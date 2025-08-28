afac6731a2357314dee0418b81d5192ce3138f46
Гульшат <gayfullinagulshat@yandex.ru>
2025-08-28 12:40:38 +0300

    add endpoint for short url

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import RedirectView
+
+from api.views import redirect_short_link
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('s/<int:pk>/', RedirectView.as_view(
-        url='https://foodgram-app.duckdns.org/recipes/%(pk)s/',
-        permanent=True
-    ), name='recipe-short-link'),
+    path('s/<int:pk>/', redirect_short_link, name='recipe-short-link'),
 ]
