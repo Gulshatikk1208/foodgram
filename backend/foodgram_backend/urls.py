a97e13cf953144c5b3989e652e25a4c6f41babab
Гульшат <gayfullinagulshat@yandex.ru>
2025-08-30 18:06:00 +0300

    test url

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import include, path
 
-from api.views import redirect_to_recipe
+# from api.views import redirect_to_recipe
+
+
+def test_view(request):
+    print("TEST VIEW CALLED!")
+    return HttpResponse("Test works!")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('s/<int:pk>/', redirect_to_recipe, name='recipe-short-link'),
+    path('test-simple/', test_view),
+    # path('s/<int:pk>/', redirect_to_recipe, name='recipe-short-link'),
 ]
