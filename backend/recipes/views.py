14366c55c29dc3ade02e724c719d904d71138616
Гульшат <gayfullinagulshat@yandex.ru>
2025-09-03 16:07:15 +0300

    fix: use ORM get() to redirect short links

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,5 +1,4 @@
 from django.http import HttpResponsePermanentRedirect, HttpResponseRedirect
-from django.shortcuts import get_object_or_404
 
 from .models import Recipe
 
@@ -7,7 +6,7 @@ from .models import Recipe
 def redirect_to_recipe(request, short_code):
     """Редирект на полную ссылку рецепта."""
     try:
-        recipe = get_object_or_404(Recipe, short_link=short_code)
+        recipe = Recipe.objects.get(short_link=short_code)
         return HttpResponsePermanentRedirect(
             request.build_absolute_uri(f'/recipes/{recipe.id}')
         )
