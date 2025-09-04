50c2d52f02e3412d85fef29954fc45a3f78a5857
Гульшат <gayfullinagulshat@yandex.ru>
2025-09-04 22:00:58 +0300

    add debug info

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -5,10 +5,13 @@ from .models import Recipe
 
 def redirect_to_recipe(request, short_code):
     """Редирект на полную ссылку рецепта."""
+    print('Функция вызвана')
     try:
         recipe = Recipe.objects.get(short_link=short_code)
+        print(f'Короткий код рецепта {recipe.id}: {recipe.short_link}')
         return HttpResponsePermanentRedirect(
             request.build_absolute_uri(f'/recipes/{recipe.id}')
         )
     except Recipe.DoesNotExist:
+        print(f'Рецепт с кодом {recipe.short_link} не найден')
         return HttpResponseRedirect(request.build_absolute_uri('/not-found'))
