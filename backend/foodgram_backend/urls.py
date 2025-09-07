aeaaf05e9692925a2729b68531ea47b538c734e8
Гульшат <gayfullinagulshat@yandex.ru>
2025-09-07 18:59:32 +0300

    remove test redirect

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -1,14 +1,10 @@
 from django.contrib import admin
-from django.http import HttpResponse
 from django.urls import include, path
 
-
-def test_shortlink_view(request, short_code):
-    return HttpResponse(f'SHORTLINK TEST OK — {short_code}', status=200)
-
+from recipes.views import redirect_to_recipe
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('s/<str:short_code>/', test_shortlink_view),
+    path('s/<str:short_code>/', redirect_to_recipe, name='recipe-short-link'),
 ]
