a9f2955eccb95728dd2a2e3eb01037b990735ae8
Гульшат <gayfullinagulshat@yandex.ru>
2025-09-07 14:44:04 +0300

    test redirect

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -7,8 +7,6 @@ def redirect_to_recipe(request, short_code):
     """Редирект на полную ссылку рецепта."""
     try:
         recipe = Recipe.objects.get(short_link=short_code)
-        return HttpResponsePermanentRedirect(
-            request.build_absolute_uri(f'/recipes/{recipe.id}/')
-        )
+        return HttpResponsePermanentRedirect(f'/recipes/{recipe.id}/')
     except Recipe.DoesNotExist:
-        return HttpResponseRedirect(request.build_absolute_uri('/not-found'))
+        return HttpResponseRedirect('/not-found')
