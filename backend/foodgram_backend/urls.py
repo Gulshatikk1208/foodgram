2ada65c3f96e2ea8cdc1b1ea40750356bde9554d
Гульшат <gayfullinagulshat@yandex.ru>
2025-09-07 17:54:57 +0300

    try easy view

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import include, path
 
-from recipes.views import redirect_to_recipe
+
+def test_shortlink_view(request, short_code):
+    return HttpResponse(f'SHORTLINK TEST OK — {short_code}', status=200)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('s/<str:short_code>/', redirect_to_recipe, name='recipe-short-link'),
+    path('s/<str:short_code>/', test_shortlink_view),
 ]
