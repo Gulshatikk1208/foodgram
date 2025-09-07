1b05fdb00d1d05535c5a0bc393a556e00bde5f09
Гульшат <gayfullinagulshat@yandex.ru>
2025-09-07 15:38:24 +0300

    test absolute url

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -7,6 +7,10 @@ def redirect_to_recipe(request, short_code):
     """Редирект на полную ссылку рецепта."""
     try:
         recipe = Recipe.objects.get(short_link=short_code)
-        return HttpResponsePermanentRedirect(f'/recipes/{recipe.id}/')
+        return HttpResponsePermanentRedirect(
+            f'https://foodgram-app.duckdns.org/recipes/{recipe.id}/'
+        )
     except Recipe.DoesNotExist:
-        return HttpResponseRedirect('/not-found')
+        return HttpResponseRedirect(
+            'https://foodgram-app.duckdns.org/not-found'
+        )
